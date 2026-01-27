7bf4b5bf00fb48792a3b3c6781a88500326c435d
agusmuss <mountaindewbeast67@gmail.com>
2026-01-28 00:07:28 +0100

    Add Firebase auth with register/login/logout

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 
 test("renders navigation", () => {
   render(
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   );
   const linkElement = screen.getByText(/Random Quotes/i);
